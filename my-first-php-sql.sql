79cc6ab2ffb56419729abae8c199d9fb271dabe6
Readyyyk <mr.sairexer@gmail.com>
2021-03-10 18:19:11 +0300

    .sql file wa added

---
+++ my-first-php-sql.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 10 2021 г., 18:17
+-- Версия сервера: 10.3.13-MariaDB-log
+-- Версия PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `my-first-php-sql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `notes`
+--
+
+CREATE TABLE `notes` (
+  `id` int(10) NOT NULL,
+  `dt` datetime NOT NULL DEFAULT current_timestamp(),
+  `date` date NOT NULL COMMENT '1000-10-10',
+  `time` time NOT NULL COMMENT '123:12:12',
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `notes`
+--
+
+INSERT INTO `notes` (`id`, `dt`, `date`, `time`, `text`, `title`) VALUES
+(24, '2021-03-10 18:17:30', '2021-03-10', '18:17:30', 'you can delete me', 'It works');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
